016fdd138f6d0a038eaa6f8f4dded159c716fafd
Matt Staudacher <mattstaudacher3@gmail.com>
2018-06-11 10:51:14 -0700

    created readme file

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName + "</span></li>");
+$("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
